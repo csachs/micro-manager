f48c1d1485d99d0e1b613f3530858000f2901fda
Christian Sachs <c.sachs@fz-juelich.de>
2017-02-06 16:28:43 +0100

    removed old approaches

--- DeviceAdapters/PyCH/PyCH.h
+++ DeviceAdapters/PyCH/PyCH.h
@@ -21,12 +21,18 @@
 #define ERR_UNKNOWN_POSITION     103
 #define ERR_IN_SEQUENCE          104
 #define ERR_SEQUENCE_INACTIVE    105
-#define ERR_STAGE_MOVING         106
-#define HUB_NOT_AVAILABLE        107
 
 namespace p = boost::python;
 
 namespace np = boost::python::numpy;
+
+template<class O, class I> O convert(I input) {
+   std::stringstream s;
+   s << input;
+   O output;
+   s >> output;
+   return output;
+};
 
 inline np::dtype dtype_conversion(int n) {
    switch(n) {
@@ -43,25 +49,35 @@ inline np::dtype dtype_conversion(int n) {
 
 class PythonImageCallback {
 public:
-   PythonImageCallback() {};
+   PythonImageCallback() : stateDevice_(0) {};
    ~PythonImageCallback() {};
 
    MM::Core *GetCoreCallback() { return callback_; }
 
    int Initialize(MM::Device *host, MM::Core *core);
-   void processImage(ImgBuffer& img_);
 
    void processBuffer(unsigned char *buffer, int channels, int height, int width, int depth);
 
-
-      void runScript(std::string name);
+   void runScript(std::string name);
 
    void updateValuesXYZ();
    void updateValuesChannelDevice();
-   void setChannelDevice(std::string channelDevice) { channelDevice_ = channelDevice; }
+
+   void setChannelDevice(std::string channelDevice) {
+      channelDevice_ = channelDevice;
+      if (channelDevice_.length() > 0) {
+         MM::State *stateDevice_ = GetCoreCallback()->GetStateDevice(host_, channelDevice_.c_str());
+      } else {
+         stateDevice_ = NULL;
+      }
+   }
+
    std::string getChannelDevice() { return channelDevice_; }
 
 private:
+
+   MM::State *stateDevice_;
+
    std::string channelDevice_;
 
    MM::Device* host_;
@@ -142,15 +158,15 @@ public:
             break;
          }
 
-         MM::Camera *cam = (MM::Camera*)GetDevice(deviceNameStr.c_str());
+         MM::Camera *camera = (MM::Camera*)GetDevice(deviceNameStr.c_str());
 
-         if (cam == this) {
+         if (camera == this) {
             continue;
          }
 
-         if(cam) {
+         if(camera) {
             cameraNames.push_back(deviceNameStr);
-            cameraDevices.push_back(cam);
+            cameraDevices.push_back(camera);
             cameraSnapState.push_back(0);
          }
       }
@@ -162,10 +178,9 @@ public:
       /* ******************************************************************** */
 
       for(long i = 0; i < channelCount_; i++) {
-         std::ostringstream os;
-         os << "Camera " << (i+1);
-         CreateProperty(os.str().c_str(), "Empty Channel", MM::String, false, new CPropertyActionEx(this, &CPyCHCamera::OnSetChannel, i), false);
-         SetAllowedValues(os.str().c_str(), cameraNames);
+         std::string cameraName = "Camera " + convert<std::string>(i+1);
+         CreateProperty(cameraName.c_str(), "Empty Channel", MM::String, false, new CPropertyActionEx(this, &CPyCHCamera::OnSetChannel, i), false);
+         SetAllowedValues(cameraName.c_str(), cameraNames);
          selectedCamera[i] = cameraDevices.size() - 1;
       }
 
@@ -215,16 +230,18 @@ public:
          MM::Camera *camera = cameraDevices[selectedCamera[channel]];
 
          if(camera != cameraDevices[index]) {
-            /*
+
             if (camera) {
                camera->RemoveTag(MM::g_Keyword_CameraChannelName);
-               camera->RemoveTag(MM::g_Keyword_CameraChannelIndex)
+               camera->RemoveTag(MM::g_Keyword_CameraChannelIndex);
             }
-            // add new labels ... (look at multicam logic)
-            */
+
+            char myName[MM::MaxStrLength];
+            GetLabel(myName);
+            camera->AddTag(MM::g_Keyword_CameraChannelName, myName, cameraNames[selectedCamera[channel]].c_str());
+            camera->AddTag(MM::g_Keyword_CameraChannelIndex, myName, convert<std::string>(channel).c_str());
 
             selectedCamera[(size_t)channel] = index;
-
          }
 
 
@@ -278,8 +295,7 @@ public:
 
    unsigned GetNumberOfChannels() const { return channelCount_; }
 
-   // TODO override this meaningfully TODO
-   int GetChannelName(unsigned channel, char* name) { CDeviceUtils::CopyLimitedString(name, ""); return DEVICE_OK; }
+   int GetChannelName(unsigned channel, char* name) { CDeviceUtils::CopyLimitedString(name, cameraNames[selectedCamera[channel]].c_str()); return DEVICE_OK; }
 
    unsigned GetBitDepth() const { return 8 * GetImageBytesPerPixel(); }
    int GetBinning() const { return 1; }
@@ -316,7 +332,7 @@ public:
 
       {
          // TODO inefficient to allocate and deallocate
-         std::vector<PyCHMultiCameraSnapThread*> camthreads(channelCount_);
+         std::vector<PyCHMultiCameraSnapThread*> cameraThreads(channelCount_);
 
          long c = 0;
          for (size_t i = 0; i < selectedCamera.size(); i++) {
@@ -326,36 +342,48 @@ public:
             if(!cameraDevices[selectedCamera[i]])
                continue;
 
-            camthreads[c] = new PyCHMultiCameraSnapThread();
+            cameraThreads[c] = new PyCHMultiCameraSnapThread();
 
-            camthreads[c]->SetCamera(cameraDevices[selectedCamera[i]]);
+            cameraThreads[c]->SetCamera(cameraDevices[selectedCamera[i]]);
             cameraSnapState[selectedCamera[i]] = 1;
 
-            camthreads[c]->Start();
+            cameraThreads[c]->Start();
 
             c++;
          }
 
-         for(size_t i = 0; i < c; i++)
-            delete camthreads[c];
+         for(size_t i = 0; i < c; i++) {
+            std::cout << "Should wait for " << i << cameraThreads[c] << " " << std::endl;
+            delete cameraThreads[c];
+         }
       }
 
       for (size_t i = 0; i < selectedCamera.size(); i++) {
          MM::Camera *camera = cameraDevices[selectedCamera[i]];
          if(camera == NULL) {
             // empty camera, create empty buffer
-            memset((void*)GetImageBuffer(i), 0, planeSize);
+            std::cout << "Zeroing " << (size_t)GetImageBuffer(i) << std::endl;
+            memset((unsigned char*)GetImageBuffer(i), 0, planeSize);
          } else {
             if(camera->GetImageWidth() == width_) {
                if(camera->GetImageHeight() == height_) {
                   assert(planeSize == camera->GetImageBufferSize());
-                  memcpy((void*)GetImageBuffer(i), camera->GetImageBuffer(), camera->GetImageBufferSize());
+                  std::cout << "Copying " << (size_t)GetImageBuffer(i) << std::endl;
+                  memcpy((unsigned char*)GetImageBuffer(i), camera->GetImageBuffer(), camera->GetImageBufferSize());
                } else {
                   const unsigned char* otherBuffer = camera->GetImageBuffer();
-                  assert(0 && "Currently not implemented");
+                  memcpy((unsigned char*)GetImageBuffer(i), camera->GetImageBuffer(), camera->GetImageBufferSize());
+                  memset((unsigned char*)GetImageBuffer(i), camera->GetImageBufferSize(), planeSize-camera->GetImageBufferSize());
                }
             } else {
-               assert(0 && "Currently not implemented");
+               memset((void*)GetImageBuffer(i), 0, planeSize);
+               for(int row = 0; row < camera->GetImageHeight(); row++) {
+                  memcpy(
+                        (unsigned char*)GetImageBuffer(i) + row*GetImageWidth() * GetImageBytesPerPixel(),
+                        camera->GetImageBuffer() + row*camera->GetImageWidth() * camera->GetImageBytesPerPixel(),
+                        std::min(GetImageWidth() * GetImageBytesPerPixel(), camera->GetImageWidth() * camera->GetImageBytesPerPixel())
+                  );
+               }
             }
          }
       }
@@ -378,8 +406,6 @@ public:
       return DEVICE_OK;
    }
 
-
-
 private:
 
    long getLongProperty(const char *name) const {
@@ -393,285 +419,5 @@ private:
 
    long bufferSize;
 };
-
-
-
-
-
-
-
-
-
-
-
-class CEmptyCam : public CCameraBase<CEmptyCam> {
-public:
-   CEmptyCam() : CCameraBase(), bufferSize(0), buffer(NULL) {
-      CreateProperty("Width", "512", MM::Integer, false);
-      CreateProperty("Height", "512", MM::Integer, false);
-
-      CreateProperty("Bytes", "1", MM::Integer, false);
-      CreateProperty("Binning", "1", MM::Integer, true);
-
-   };
-   ~CEmptyCam() {};
-
-   int Initialize() { return DEVICE_OK; };
-   int Shutdown() { return DEVICE_OK; };
-   void GetName(char *name) const { CDeviceUtils::CopyLimitedString(name, "EmptyCam"); };
-
-   long GetImageBufferSize() const { return bufferSize; }
-   unsigned GetBitDepth() const { return 8 * GetImageBytesPerPixel(); }
-   int GetBinning() const { return 1; }
-   int SetBinning(int binSize) { return DEVICE_OK; }
-   void SetExposure(double exp_ms) { return ; }
-   double GetExposure() const { return 1.0; }
-   int SetROI(unsigned x, unsigned y, unsigned xSize, unsigned ySize) { return DEVICE_OK; }
-   int GetROI(unsigned& x, unsigned& y, unsigned& xSize, unsigned& ySize) { x = 0; y = 0; xSize = GetImageWidth(); ySize = GetImageHeight(); return DEVICE_OK; }
-
-   int ClearROI() { return DEVICE_OK; }
-
-   int IsExposureSequenceable(bool& isSequenceable) const { isSequenceable = false; return DEVICE_OK; }
-
-
-   const unsigned char* GetImageBuffer() {
-      long currentSize = GetImageWidth() * GetImageHeight() * GetImageBytesPerPixel();
-      if (currentSize != bufferSize) {
-         if (buffer) {
-            // potential double free if malloc fails
-            delete buffer;
-         }
-
-         buffer = new unsigned char[currentSize];
-         bufferSize = currentSize;
-
-         memset(buffer, 0, bufferSize);
-      }
-
-      return buffer;
-   }
-
-   unsigned GetImageWidth() const { return getLongProperty("Width"); }
-
-   unsigned GetImageHeight() const { return getLongProperty("Height"); }
-
-   unsigned GetImageBytesPerPixel() const { return getLongProperty("Bytes"); }
-
-   int SnapImage() { return DEVICE_OK; };
-private:
-
-
-   long getLongProperty(const char *name) const {
-      char buf[MM::MaxStrLength];
-      GetProperty(name, buf);
-      return atoi(buf);
-   }
-
-   unsigned char *buffer;
-   long bufferSize;
-};
-
-class MySequenceThread;
-class CPyCH : public CCameraBase<CPyCH>
-{
-public:
-   CPyCH();
-   ~CPyCH();
-
-   // MMDevice API
-   // ------------
-   int Initialize();
-   int Shutdown();
-
-   void GetName(char* name) const;
-
-   // MMCamera API
-   // ------------
-   int SnapImage();
-   const unsigned char* GetImageBuffer();
-   unsigned GetImageWidth() const;
-   unsigned GetImageHeight() const;
-   unsigned GetImageBytesPerPixel() const;
-   unsigned GetBitDepth() const;
-   long GetImageBufferSize() const;
-   double GetExposure() const;
-   void SetExposure(double exp);
-   int SetROI(unsigned x, unsigned y, unsigned xSize, unsigned ySize);
-   int GetROI(unsigned& x, unsigned& y, unsigned& xSize, unsigned& ySize);
-   int ClearROI();
-   int PrepareSequenceAcqusition() { return DEVICE_OK; }
-   int StartSequenceAcquisition(double interval);
-   int StartSequenceAcquisition(long numImages, double interval_ms, bool stopOnOverflow);
-   int StopSequenceAcquisition();
-   int InsertImage();
-   int RunSequenceOnThread(MM::MMTime startTime);
-   bool IsCapturing();
-   void OnThreadExiting() throw();
-   double GetNominalPixelSizeUm() const {return nominalPixelSizeUm_;}
-   double GetPixelSizeUm() const {return nominalPixelSizeUm_ * GetBinning();}
-   int GetBinning() const;
-   int SetBinning(int bS);
-   int IsExposureSequenceable(bool& isSequenceable) const;
-   int GetExposureSequenceMaxLength(long& nrEvents) { nrEvents = 0; return DEVICE_OK; };
-   int StartExposureSequence() { return DEVICE_ERR; }
-   int StopExposureSequence() { return DEVICE_ERR; }
-   int ClearExposureSequence() { return DEVICE_ERR; }
-   int AddToExposureSequence(double exposureTime_ms) { return DEVICE_ERR; }
-   int SendExposureSequence() { return DEVICE_ERR; }
-   unsigned  GetNumberOfComponents() const { return nComponents_;};
-   // action interface
-   // ----------------
-   int OnScript(MM::PropertyBase *pProp, MM::ActionType eAct);
-   int OnChannelDevice(MM::PropertyBase* pProp, MM::ActionType eAct);
-   int OnCameraDevice(MM::PropertyBase* pProp, MM::ActionType eAct);
-   int OnPixelType(MM::PropertyBase* pProp, MM::ActionType eAct);
-   int OnBitDepth(MM::PropertyBase* pProp, MM::ActionType eAct);
-   int OnCameraCCDXSize(MM::PropertyBase* , MM::ActionType );
-   int OnCameraCCDYSize(MM::PropertyBase* , MM::ActionType );
-private:
-   bool PerformCallback(ImgBuffer &img);
-   int ResizeImageBuffer();
-   static const double nominalPixelSizeUm_;
-
-   PythonImageCallback pyc_;
-
-   std::string scriptFile_;
-   std::string channelDevice_;
-   std::string cameraDevice_;
-
-   ImgBuffer img_;
-   bool initialized_;
-   double readoutUs_;
-   MM::MMTime readoutStartTime_;
-   int bitDepth_;
-   unsigned roiX_;
-   unsigned roiY_;
-   MM::MMTime sequenceStartTime_;
-   long imageCounter_;
-   long binSize_;
-   long cameraCCDXSize_;
-   long cameraCCDYSize_;
-   std::string triggerDevice_;
-   bool stopOnOverflow_;
-   MMThreadLock imgPixelsLock_;
-   friend class MySequenceThread;
-   int nComponents_;
-   MySequenceThread * thd_;
-};
-
-class MySequenceThread : public MMDeviceThreadBase
-{
-   friend class CPyCH;
-   enum { default_numImages=1, default_intervalMS = 100 };
-public:
-   MySequenceThread(CPyCH* pCam);
-   ~MySequenceThread();
-   void Stop();
-   void Start(long numImages, double intervalMs);
-   bool IsStopped();
-   void Suspend();
-   bool IsSuspended();
-   void Resume();
-   double GetIntervalMs(){return intervalMs_;}
-   void SetLength(long images) {numImages_ = images;}
-   long GetLength() const {return numImages_;}
-   long GetImageCounter(){return imageCounter_;}
-   MM::MMTime GetStartTime(){return startTime_;}
-   MM::MMTime GetActualDuration(){return actualDuration_;}
-private:
-   int svc(void) throw();
-   double intervalMs_;
-   long numImages_;
-   long imageCounter_;
-   bool stop_;
-   bool suspend_;
-   CPyCH* camera_;
-   MM::MMTime startTime_;
-   MM::MMTime actualDuration_;
-   MM::MMTime lastFrameTime_;
-   MMThreadLock stopLock_;
-   MMThreadLock suspendLock_;
-};
-
-
-#define MAX_NUMBER_PHYSICAL_CAMERAS       5
-
-
-#define ERR_INVALID_DEVICE_NAME            10001
-
-#define ERR_NO_PHYSICAL_CAMERA             10010
-#define ERR_NO_EQUAL_SIZE                  10011
-
-
-/**
- * CameraSnapThread: helper thread for MultiCamera
- */
-
-/*
- * CMultiCameraPyCH: Combines multiple physical cameras into one logical device
- */
-class CMultiCameraPyCH : public CCameraBase<CMultiCameraPyCH>
-{
-public:
-   CMultiCameraPyCH();
-   ~CMultiCameraPyCH();
-
-   int Initialize();
-   int Shutdown();
-
-   void GetName(char* name) const;
-
-   int SnapImage();
-   const unsigned char* GetImageBuffer();
-   const unsigned char* GetImageBuffer(unsigned channelNr);
-   unsigned GetImageWidth() const;
-   unsigned GetImageHeight() const;
-   unsigned GetImageBytesPerPixel() const;
-   unsigned GetBitDepth() const;
-   long GetImageBufferSize() const;
-   double GetExposure() const;
-   void SetExposure(double exp);
-   int SetROI(unsigned x, unsigned y, unsigned xSize, unsigned ySize);
-   int GetROI(unsigned& x, unsigned& y, unsigned& xSize, unsigned& ySize);
-   int ClearROI();
-   int PrepareSequenceAcqusition();
-   int StartSequenceAcquisition(double interval);
-   int StartSequenceAcquisition(long numImages, double interval_ms, bool stopOnOverflow);
-   int StopSequenceAcquisition();
-   int GetBinning() const;
-   int SetBinning(int bS);
-   int IsExposureSequenceable(bool& isSequenceable) const;
-   unsigned  GetNumberOfComponents() const;
-   unsigned  GetNumberOfChannels() const;
-   int GetChannelName(unsigned channel, char* name);
-   bool IsCapturing();
-
-   // action interface
-   // ---------------
-   int OnPhysicalCamera(MM::PropertyBase* pProp, MM::ActionType eAct, long nr);
-   int OnBinning(MM::PropertyBase* pProp, MM::ActionType eAct);
-
-   int OnScript(MM::PropertyBase *pProp, MM::ActionType eAct);
-
-      private:
-   int Logical2Physical(int logical);
-   bool ImageSizesAreEqual();
-   unsigned char* imageBuffer_;
-
-   std::string scriptFile_;
-   PythonImageCallback pyc_;
-
-
-   std::vector<std::string> availableCameras_;
-   std::vector<std::string> usedCameras_;
-   std::vector<int> cameraWidths_;
-   std::vector<int> cameraHeights_;
-   std::vector<MM::Camera*> physicalCameras_;
-   unsigned int nrCamerasInUse_;
-   bool initialized_;
-   ImgBuffer img_;
-};
-
-
 
 #endif //_PYCH_H_
